efe6feda5be9c8055d29a06e27a3965c2d8b7183
Dariia Gaydukova <daria1171@gmail.com>
2022-09-23 21:04:11 +0300

    Изменинила метод WordsChecker

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -5,35 +5,21 @@ import java.util.Set;
 public class WordsChecker {
 
     protected String text;
-    Set<String> textCollection;
-
-
-    public String getText(String s) {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
+    Set<String> textCollection = new HashSet<>();
 
 
     public WordsChecker(String text) {
         this.text = text;
-        Set<String> textCollection = new HashSet<>();
-        textCollection.add(String.valueOf(text.split("\\P{IsAlphabetic}+")));
-        this.textCollection = textCollection;
-
+        String[] words = text.split("\\P{IsAlphabetic}+");
+        for (String word : words) {
+            textCollection.add(word);
+        }
 
     }
 
 
     public boolean hasWord(String word) {
-        if (textCollection.contains(word)) {
-            return true;
-        } else {
-            return false;
-        }
+        return textCollection.contains(word);
     }
 
 
